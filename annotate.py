9ec23a055a228aec5df31d0fbecb55d4b5a9d737
Woze Parrot <wozeparrot@gmail.com>
2024-02-07 17:00:51 -0500

    feat: annotate number and color

--- annotate.py
+++ annotate.py
@@ -26,16 +26,65 @@ while not flag and i < len(frame_files):
     cv2.imshow("preview", frame)
 
     key = cv2.waitKey(1)
-    if key == ord("q"):
+    if key == ord("z"):
       flag = True
       break
-    elif key == ord("n"):
+    elif key == ord("0"):
       with open(Path(frame_file).with_suffix(".txt"), "w") as f:
         f.write("0")
       break
-    elif key == ord("c"):
+    # red
+    elif key == ord("1"):
       with open(Path(frame_file).with_suffix(".txt"), "w") as f:
         f.write("1")
+      break
+    elif key == ord("2"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("2")
+      break
+    elif key == ord("3"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("3")
+      break
+    elif key == ord("4"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("4")
+      break
+    elif key == ord("5"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("5")
+      break
+    elif key == ord("6"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("6")
+      break
+    # blue
+    elif key == ord("q"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("7")
+      break
+    elif key == ord("w"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("8")
+      break
+    elif key == ord("e"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("9")
+      break
+    elif key == ord("r"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("10")
+      break
+    elif key == ord("t"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("11")
+      break
+    elif key == ord("y"):
+      with open(Path(frame_file).with_suffix(".txt"), "w") as f:
+        f.write("12")
+      break
+    # skip and back
+    elif key == ord("s"):
       break
     elif key == ord("d"):
       i -= 2
